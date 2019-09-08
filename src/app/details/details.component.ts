1a16f90b0caea02ac02a91006807831cf680f5b9
mittulmandhan <conanmittul@gmail.com>
2019-09-08 19:52:40 +0530

    class3: component lifecycle hooks

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,21 +1,39 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, OnDestroy, DoCheck } from '@angular/core';
 
+
+// angular is suggesting us not to use ngDoCheck hook because angular internally handles it
+// Thats why we are disabling the error shown by angular
+// tslint:disable-next-line:no-conflicting-lifecycle
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styles: []
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnChanges, OnDestroy, DoCheck {
   @Input() myCompany: string;
   @Output() myMessage = new EventEmitter<string>();
-constructor() {
+  constructor() {
+    console.log(`child constructor`);
+  }
+
+  ngOnChanges() {
+    console.log(`child ngOnChanges`);
   }
 
   ngOnInit() {
+    console.log(`child ngOnInit`);
   }
 
 Greet() {
     this.myMessage.emit(`Hi, from details!`);
+  }
+
+  ngDoCheck() {
+    console.log(`child ngDoCheck`);
+  }
+
+  ngOnDestroy() {
+    console.log(`child ngOnDestroy`);
   }
 
 }
