bad57b05bfaba303683ac4a79b30d03eb291a733
mittulmandhan <conanmittul@gmail.com>
2019-09-09 23:55:18 +0530

    custom model driven form validations (error resolved)

--- src/app/modelform/modelform.component.ts
+++ src/app/modelform/modelform.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
+function ValidateUrl(control: FormControl) {
+  if (!control.value.startsWith('https')) {
+    return { inValidUrl: true };
+  }
+}
+
 @Component({
   selector: 'app-modelform',
   templateUrl: './modelform.component.html',
@@ -9,33 +15,27 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 export class ModelformComponent implements OnInit {
   userForm: FormGroup;
   constructor(private fb: FormBuilder) {
-    this.userForm = fb.group({
-      name: ['', [Validators.required, Validators.minLength(3)]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', Validators.required],
-      contactNo: ['', [Validators.required, Validators.pattern(`^[6,7,8,9]\\d{9}$`)]],
-      Url: [Validators.required.ValidateUrl],
-      terms: ['', [Validators.required]]
-    });
+    this.userForm = fb.group(
+      {
+        name: ['', [Validators.required, Validators.minLength(3)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required],
+        contact: ['', [Validators.pattern('^[6,7,8,9]\\d{9}$')]],
+        url: ['', [Validators.required, ValidateUrl]],
+        terms: ['', [Validators.required]],
+      });
 
-    this.userForm.get('password').hasError('validateEqual');
+    // this.userForm.get('confirmPassword').hasError('validateEqual')
   }
 
   ngOnInit() {
-  }
 
+  }
   SaveData() {
     if (this.userForm.valid) {
-      console.log(`form is valid!`);
+      console.log('form is valid!');
       console.log(this.userForm.value);
     }
-  }
-
-}
-
-function ValidateUrl(control: FormControl) {
-  if (control.value.startsWith('https')) {
-    return {invalidUrl: true} ;
   }
 }
